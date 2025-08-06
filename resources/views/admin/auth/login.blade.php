bda82a5396f9f80a383172b8be0edb5eaca3bb8d
gbtan23 <gilbert.tan1991@gmail.com>
2025-08-06 21:19:27 +0800

    Add the commonGrid design that will be used in the login page

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,8 +2,13 @@
 
 
     {{-- PAGE WRAPPER --}}
-    <div>
-        
+    <div class="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+        <div class="relative flex flex-col justify-center w-full h-screen dark:bg-gray-900 lg:flex-row">
+
+            <x-partials.admin.auth.common-grid />
+
+
+        </div>
     </div>
 
 
