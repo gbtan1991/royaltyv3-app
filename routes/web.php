e627823af256094562f163c9361aa15993162d19
gbtan23 <gilbert.tan1991@gmail.com>
2025-08-06 21:20:49 +0800

    Added the redirect and middleware

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,17 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/login', function(){
+    return redirect()->route('admin.login');
+})->name('login');
+
+
 Route::get('/', [DashboardController::class, 'index'])
+->middleware('auth:admin')
 ->name('dashboard');
+
+
+
 
 require __DIR__.'/admin/auth.php';
