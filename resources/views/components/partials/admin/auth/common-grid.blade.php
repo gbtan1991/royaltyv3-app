65d6e8e204edf398acc382ffb7f05ebd79d61078
gbtan23 <gilbert.tan1991@gmail.com>
2025-08-06 21:20:31 +0800

    Created the commongrid component design

---
+++ resources/views/components/partials/admin/auth/common-grid.blade.php
@@ -0,0 +1,16 @@
+<div class="relative items-center hidden w-full h-full bg-brand-950 dark:bg-white/5 lg:grid lg:w-1/2">
+    <div class="flex items-center justify-center z-1">
+        <!-- ===== Common Grid Shape Start ===== -->
+        <x-partials.admin.auth.common-grid-shape/>
+
+        <div class="flex flex-col items-center max-w-xs">
+            <a href="index.html" class="block mb-4">
+                <img src="{{ asset('images/crown-white.png')}}" alt="Logo" class="w-60 -mb-10" />
+            </a>
+            <h1 class="text-3xl font-bold text-white">Royalty Rewards App</h1>
+            <p class="text-center text-gray-400 dark:text-white/60">
+                Points Made Powerful. Rewards Made Easy.
+            </p>
+        </div>
+    </div>
+</div>
