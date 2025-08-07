41947c7989aa959a70ddccf1b6757d6f27f8575f
gbtan23 <gilbert.tan1991@gmail.com>
2025-08-07 20:29:14 +0800

    Created reusable input component

---
+++ app/View/Components/ui/AppInput.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components\ui;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class AppInput extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $name,
+        public string $type = 'text',
+        public string $placeholder = '',
+        public string $value = '',
+        public bool $required = false
+    ){}
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.ui.app-input');
+    }
+}
---
+++ resources/views/components/ui/app-input.blade.php
@@ -0,0 +1,17 @@
+<input
+    type="{{ $type }}"
+    id="{{ $name }}"
+    name="{{ $name }}"
+    placeholder="{{ $placeholder }}"
+    value="{{ old($name, $value) }}"
+    @if($required) required @endif
+
+    {{ $attributes->merge([
+        'class' =>
+            'dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800'
+    ]) }}
+/>
+
+@error($name)
+    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+@enderror
