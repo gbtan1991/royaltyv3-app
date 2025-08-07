47a8e43d9ce7f7438240acb2f3bc0d7553ed23d4
gbtan23 <gilbert.tan1991@gmail.com>
2025-08-07 20:30:08 +0800

    Created the loginForm component that will be called by the login page

---
+++ resources/views/components/partials/admin/auth/login-form.blade.php
@@ -0,0 +1,43 @@
+<x-ui.app-form method="POST" action="{{ route('admin.login') }}">
+
+    {{-- Username --}}
+    <div class="mb-5">
+        <x-ui.app-label for="username" required>
+            Username
+        </x-ui.app-label>
+
+        <x-ui.app-input 
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            required
+            class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300"
+        />
+    </div>
+
+    {{-- Password --}}
+    <div class="mb-5">
+        <x-ui.app-label for="password" required>
+            Password
+        </x-ui.app-label>
+
+        <x-ui.app-input 
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            required
+            class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+        />
+    </div>
+
+    {{-- Submit --}}
+    <div class="mt-6">
+
+        <x-ui.app-button type="submit" class="w-full h-11 px-6 bg-brand-600 text-white rounded-lg hover:bg-brand-700 transition">
+                Sign In
+        </x-ui.app-button>
+
+       
+    </div>
+
+</x-ui.app-form>
